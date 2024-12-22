04ed48787b1e33d73b2327cc1efd37751fa91b21
Samuel Santos <samuel_a_santos@hotmail.com>
2024-12-22 10:42:27 -0300

    Atualizado script.js
    - Adicionado a função com a lógica do display
    - Adicionado a função do botão C com reset das variáveis

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
+let display = document.querySelector('#display');
+
+let firstValue = '';
+let secondValue = '';
+let operator = '';
+let currentValue = 1;
+
+function reset() {
+    firstValue = '';
+    secondValue = '';
+    operator = '';
+    currentValue = 1;
+}
+
 function clicked(button) {
     switch (button) {
         case "c":
             // Limpa tudo
+            reset();
             break;
         case "÷":
         case "×":
@@ -31,5 +46,15 @@ function clicked(button) {
         default:
             // Caso algo diferente seja digitado exibir o erro
             break;
+    }
+
+    updateDisplay();
+}
+
+function updateDisplay() {
+    if (firstValue === '') {
+        display.innerHTML = '0';
+    } else {
+        display.innerHTML = firstValue + operator + secondValue;
     }
 }
