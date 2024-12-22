3c415824273c2ebff5033ce9e8cbed58a8127739
Samuel Santos <samuel_a_santos@hotmail.com>
2024-12-21 22:48:35 -0300

    Adicionado suporte para interatividade inicial na calculadora.
    - Atualizado o HTML para incluir a função `clicked` nos botões da calculadora, permitindo a captura de cliques.
    - Implementado a função `clicked` no JavaScript com um `switch` para gerenciar ações dos botões:
      - Limpeza (`C`).
      - Operações matemáticas (`+`, `-`, `×`, `÷`).
      - Inserção de números (`0-9`).
      - Tratamento de pontos decimais (`.`).
      - Cálculo do resultado (`=`).
      - Tratamento de entradas inválidas.
    - Preparado o código para exibir os números e realizar as operações no futuro.

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function clicked(button) {
+    switch (button) {
+        case "c":
+            // Limpa tudo
+            break;
+        case "÷":
+        case "×":
+        case "+":
+        case "-":
+            // Escolhe operações
+            break;
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+            // Exibe os números
+            break;
+        case ".":
+            // Verificar se já tem um primeiro número digitado
+            // Verificar se já existe um outro ponto
+            break;
+        case "=":
+            // Exibe o resultado da conta no display
+            break;
+        default:
+            // Caso algo diferente seja digitado exibir o erro
+            break;
+    }
+}
